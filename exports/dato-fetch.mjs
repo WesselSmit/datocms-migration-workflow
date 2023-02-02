50cd52ee62e4cc4452229c4d5a7bbcaf84cbab0b
wesselsmit <wrsmit00@gmail.com>
2023-02-02 16:14:40 +0100

    fix: import paths

--- exports/dato-fetch.mjs
+++ exports/dato-fetch.mjs
@@ -1,6 +1,6 @@
-import { datoContentRequest } from '../lib/dato-request.mjs'
-import { getState } from '../lib/state-helpers.mjs'
-import { STATE_FILE_NAME } from '../lib/constants.mjs'
+import { datoContentRequest } from '../src/lib/dato-request.mjs'
+import { getState } from '../src/lib/state-helpers.mjs'
+import { STATE_FILE_NAME } from '../src/lib/constants.mjs'
 
 export default async function datoFetch(query, options, useEnvFromState = true) {
   if (!query) {
@@ -13,6 +13,9 @@ export default async function datoFetch(query, options, useEnvFromState = true) 
     if (envFromState) {
       console.log(`Using "${envFromState}" as datocms environment to fetch from (found in ${STATE_FILE_NAME}).\n`)
       options.url = `https://graphql.datocms.com/environments/${envFromState}`
+    } else {
+      console.log(`No "currentEnv" specified in ${STATE_FILE_NAME} or ${STATE_FILE_NAME} does not exist.`)
+      console.log(`Using the default environment instead.\n`)
     }
   }
 
