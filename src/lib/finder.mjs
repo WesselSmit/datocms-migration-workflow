c29d49e5c47982f4b550567bc3a9ce5713251389
wesselsmit <wrsmit00@gmail.com>
2023-02-03 15:12:45 +0100

    test: import state from dependant root

--- src/lib/finder.mjs
+++ src/lib/finder.mjs
@@ -3,26 +3,37 @@ import { dirname, join, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 
-export const APP_ROOT = findAppRoot()
-export const DEPENDENT_APP_ROOT = findDirOfFileLocation(resolve(APP_ROOT, '..'), 'package.json')
-
-
-function findAppRoot() {
+export const APP_ROOT = (() => {
   const currentFilename = fileURLToPath(import.meta.url)
-  const pathToRoot = resolve(currentFilename, '../../')
+  const pathToRoot = resolve(currentFilename, '..', '..')
 
   return dirname(pathToRoot)
-}
+})()
 
-function findDirOfFileLocation(dir, filename) {
-  const ls = readdirSync(dir)
-  const hasStateFile = ls.includes(filename)
+export const DEPENDENT_APP_ROOT = (() => {
+  function findDirOfFileLocation(dir, filename) {
+    const ls = readdirSync(dir)
+    const hasStateFile = ls.includes(filename)
 
-  if (hasStateFile) {
-    return dir
-  } else if (dir === '/') {
-    throw new Error(`Could not find a ${filename} in project root.`)
-  } else {
-    return findDirOfFileLocation(resolve(dir, '..'), filename)
+    if (hasStateFile) {
+      return dir
+    } else if (dir === '/') {
+      throw new Error(`Could not find a ${filename} in project root.`)
+    } else {
+      const newDir = resolve(dir, '..')
+      return findDirOfFileLocation(newDir, filename)
+    }
   }
+
+  // '..' is used to ensure it doesn't 'find' the package.json of the datocms-migration-workflow package itself
+  const startingDir = resolve(APP_ROOT, '..')
+
+  return findDirOfFileLocation(startingDir, 'package.json')
+})()
+
+export async function importJsFileFromDependantApp(filename) {
+  const filePath = join(DEPENDENT_APP_ROOT, filename)
+  const file = await import(filePath)
+
+  return file
 }
