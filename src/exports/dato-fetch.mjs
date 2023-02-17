2d9d4910304cf697d4974832c000b62ce860ffbc
wesselsmit <wrsmit00@gmail.com>
2023-02-17 14:53:51 +0100

    Revert "feat: use named export instead of default export for datoFetch"
    This reverts commit 700f38b76085390e1d2153ab7e5a7831dd1167f6.

--- src/exports/dato-fetch.mjs
+++ src/exports/dato-fetch.mjs
@@ -3,7 +3,8 @@ import { getState } from '../lib/state-helpers.mjs'
 import { STATE_FILE_NAME } from '../lib/constants.mjs'
 
 
-export default async function datoFetch(query, options) {
+// todo 'useEnvFromState' should be part of 'options'
+export default async function datoFetch(query, options, useEnvFromState = true) {
   if (!query) {
     throw new Error('Please pass a query to the datoFetch function.')
   }
@@ -13,15 +14,15 @@ export default async function datoFetch(query, options) {
       ...options.headers,
       'X-Environment': options.env,
     }
-  } else if (options?.useState) {
+  } else if (useEnvFromState) {
     const { currentEnv: envFromState } = await getState()
 
     if (envFromState) {
+      console.log(`Using "${envFromState}" as datocms environment to fetch from (found in ${STATE_FILE_NAME}).\n`)
       options.headers = {
         ...options.headers,
         'X-Environment': envFromState,
       }
-      console.log(`Using "${envFromState}" as datocms environment to fetch from (found in ${STATE_FILE_NAME}).\n`)
     } else {
       console.log(`No "currentEnv" specified in ${STATE_FILE_NAME} or ${STATE_FILE_NAME} does not exist.`)
       console.log(`Using the default environment instead.\n`)
@@ -29,12 +30,14 @@ export default async function datoFetch(query, options) {
   }
 
   try {
+    console.log('QUERY ::', query)
+    console.log('OPTIONS ::', options)
     const data = await datoContentRequest(query, options)
+    console.log('DATA ::', data)
 
     return data
   } catch (error) {
     console.error(error)
-
     return error
   }
 }
